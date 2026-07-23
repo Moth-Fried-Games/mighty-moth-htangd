dc134b3a2b9bffc593642908157bf98fa2287041
Myr the Moth <myrthemoth@gmail.com>
2026-07-23 18:51:46 +0100

    setup setup setup

--- game/title/menu.gd
+++ game/title/menu.gd
@@ -1,72 +1,59 @@
 extends CanvasLayer
 
-var menu_music: AudioStreamPlayer = null
-
 @onready var tab_container: TabContainer = $Control/TabContainer
 
 # Main Menu
-@onready var start_button: Button = $Control/TabContainer/Main/VBoxContainer/StartButton
-@onready var how_button: Button = $Control/TabContainer/Main/VBoxContainer/HowButton
-@onready var settings_button: Button = $Control/TabContainer/Main/VBoxContainer/SettingsButton
-@onready var credits_button: Button = $Control/TabContainer/Main/VBoxContainer/CreditsButton
-@onready var quit_button: Button = $Control/TabContainer/Main/VBoxContainer/QuitButton
+@onready var main_start_button: Button = %MainStartButton
+@onready var main_how_button: Button = %MainHowButton
+@onready var main_settings_button: Button = %MainSettingsButton
+@onready var main_credits_button: Button = %MainCreditsButton
+@onready var main_quit_button: Button = %MainQuitButton
 
 # How to Play
-@onready var howto_return_button: Button = $Control/TabContainer/HowTo/VBoxContainer/ReturnButton
+@onready var how_to_return_button: Button = %HowToReturnButton
 
 # Settings
-@onready
-var settings_return_button: Button = $Control/TabContainer/Settings/VBoxContainer/ReturnButton
-@onready
-var master_h_slider: HSlider = $Control/TabContainer/Settings/VBoxContainer/PanelContainer/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer/MasterHSlider
-@onready
-var master_label: Label = $Control/TabContainer/Settings/VBoxContainer/PanelContainer/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer/MasterLabel
-@onready
-var music_h_slider: HSlider = $Control/TabContainer/Settings/VBoxContainer/PanelContainer/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer3/MusicHSlider
-@onready
-var music_label: Label = $Control/TabContainer/Settings/VBoxContainer/PanelContainer/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer3/MusicLabel
-@onready
-var sound_h_slider: HSlider = $Control/TabContainer/Settings/VBoxContainer/PanelContainer/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer4/SoundHSlider
-@onready
-var sound_label: Label = $Control/TabContainer/Settings/VBoxContainer/PanelContainer/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer4/SoundLabel
-@onready
-var display_option_button: OptionButton = $Control/TabContainer/Settings/VBoxContainer/PanelContainer/VBoxContainer/HBoxContainer/VBoxContainer2/DisplayOptionButton
-@onready
-var v_sync_option_button: OptionButton = $Control/TabContainer/Settings/VBoxContainer/PanelContainer/VBoxContainer/HBoxContainer/VBoxContainer2/VSyncOptionButton
-@onready
-var frame_cap_option_button: OptionButton = $Control/TabContainer/Settings/VBoxContainer/PanelContainer/VBoxContainer/HBoxContainer/VBoxContainer2/FrameCapOptionButton
-@onready
-var frame_label_option_button: OptionButton = $Control/TabContainer/Settings/VBoxContainer/PanelContainer/VBoxContainer/HBoxContainer/VBoxContainer2/FrameLabelOptionButton
+@onready var settings_master_h_slider: HSlider = %SettingsMasterHSlider
+@onready var settings_master_label: Label = %SettingsMasterLabel
+@onready var settings_music_h_slider: HSlider = %SettingsMusicHSlider
+@onready var settings_music_label: Label = %SettingsMusicLabel
+@onready var settings_sound_h_slider: HSlider = %SettingsSoundHSlider
+@onready var settings_sound_label: Label = %SettingsSoundLabel
+@onready var settings_display_option_button: OptionButton = %SettingsDisplayOptionButton
+@onready var settings_v_sync_option_button: OptionButton = %SettingsVSyncOptionButton
+@onready var settings_frame_cap_option_button: OptionButton = %SettingsFrameCapOptionButton
+@onready var settings_frame_label_option_button: OptionButton = %SettingsFrameLabelOptionButton
+@onready var settings_return_button: Button = %SettingsReturnButton
 
 # Credits
-@onready var credits_return_button: Button = $Control/TabContainer/Credits/VBoxContainer/ReturnButton
-@onready
-var credits_rich_text_label: RichTextLabel = $Control/TabContainer/Credits/VBoxContainer/PanelContainer/VBoxContainer/RichTextLabel
+@onready var credits_return_button: Button = %CreditsReturnButton
+@onready var credits_rich_text_label: RichTextLabel = %CreditsRichTextLabel
 
 var input_ready: bool = false
 
 
 func _ready() -> void:
 	load_settings()
-	start_button.pressed.connect(change_to_game)
-	quit_button.pressed.connect(quit_game)
-	how_button.pressed.connect(howto_menu)
-	settings_button.pressed.connect(settings_menu)
-	credits_button.pressed.connect(credits_menu)
-	howto_return_button.pressed.connect(main_menu)
+	main_start_button.pressed.connect(change_to_game)
+	main_quit_button.pressed.connect(quit_game)
+	main_how_button.pressed.connect(howto_menu)
+	main_settings_button.pressed.connect(settings_menu)
+	main_credits_button.pressed.connect(credits_menu)
+	how_to_return_button.pressed.connect(main_menu)
 	settings_return_button.pressed.connect(main_menu)
-	master_h_slider.value_changed.connect(_on_master_value_changed)
-	music_h_slider.value_changed.connect(_on_music_value_changed)
-	sound_h_slider.value_changed.connect(_on_sound_value_changed)
-	display_option_button.item_selected.connect(_on_display_item_selected)
-	v_sync_option_button.item_selected.connect(_on_vsync_item_selected)
-	frame_cap_option_button.item_selected.connect(_on_framecap_item_selected)
-	frame_label_option_button.item_selected.connect(_on_framelabel_item_selected)
+	settings_master_h_slider.value_changed.connect(_on_master_value_changed)
+	settings_music_h_slider.value_changed.connect(_on_music_value_changed)
+	settings_sound_h_slider.value_changed.connect(_on_sound_value_changed)
+	settings_display_option_button.item_selected.connect(_on_display_item_selected)
+	settings_v_sync_option_button.item_selected.connect(_on_vsync_item_selected)
+	settings_frame_cap_option_button.item_selected.connect(_on_framecap_item_selected)
+	settings_frame_label_option_button.item_selected.connect(_on_framelabel_item_selected)
 	credits_return_button.pressed.connect(main_menu)
 	credits_rich_text_label.meta_clicked.connect(credits_click_link)
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	GameUi.ui_transitions.toggle_transition(false)
-	menu_music = GameGlobals.audio_manager.create_persistent_audio("music_menu")
+	if not GameGlobals.audio_manager.persistent_audio.has("music_menu"):
+		GameGlobals.audio_manager.create_persistent_audio("music_menu")
 	tab_container.current_tab = 0
 
 
@@ -106,8 +93,7 @@ func change_to_game() -> void:
 	if input_ready:
 		input_ready = false
 		GameGlobals.audio_manager.create_audio("sound_menu_click")
-		if is_instance_valid(menu_music):
-			GameGlobals.audio_manager.fade_audio_out_and_destroy("music_menu", menu_music, 1)
+		GameGlobals.audio_manager.fade_persistent_audio_out_and_destroy("music_menu", 1)
 		GameUi.ui_transitions.change_scene_with_loading(GameGlobals.lab_scene)
 
 
@@ -116,56 +102,56 @@ func quit_game() -> void:
 
 
 func load_settings() -> void:
-	master_h_slider.value = (GameGlobals.game_settings.master_volume * 100)
-	master_label.text = str("%d" % [GameGlobals.game_settings.master_volume * 100])
-	music_h_slider.value = (GameGlobals.game_settings.music_volume * 100)
-	music_label.text = str("%d" % [GameGlobals.game_settings.music_volume * 100])
-	sound_h_slider.value = (GameGlobals.game_settings.sound_volume * 100)
-	sound_label.text = str("%d" % [GameGlobals.game_settings.sound_volume * 100])
+	settings_master_h_slider.value = (GameGlobals.game_settings.master_volume * 100)
+	settings_master_label.text = str("%d" % [GameGlobals.game_settings.master_volume * 100])
+	settings_music_h_slider.value = (GameGlobals.game_settings.music_volume * 100)
+	settings_music_label.text = str("%d" % [GameGlobals.game_settings.music_volume * 100])
+	settings_sound_h_slider.value = (GameGlobals.game_settings.sound_volume * 100)
+	settings_sound_label.text = str("%d" % [GameGlobals.game_settings.sound_volume * 100])
 	match GameGlobals.game_settings.display_mode:
 		DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN:
-			display_option_button.selected = 0
+			settings_display_option_button.selected = 0
 		DisplayServer.WINDOW_MODE_WINDOWED:
-			display_option_button.selected = 1
+			settings_display_option_button.selected = 1
 	match GameGlobals.game_settings.vsync_mode:
 		DisplayServer.VSYNC_DISABLED:
-			v_sync_option_button.selected = 0
+			settings_v_sync_option_button.selected = 0
 		DisplayServer.VSYNC_ENABLED:
-			v_sync_option_button.selected = 1
+			settings_v_sync_option_button.selected = 1
 		DisplayServer.VSYNC_ADAPTIVE:
-			v_sync_option_button.selected = 2
+			settings_v_sync_option_button.selected = 2
 		DisplayServer.VSYNC_MAILBOX:
-			v_sync_option_button.selected = 3
+			settings_v_sync_option_button.selected = 3
 	match GameGlobals.game_settings.frame_rate_cap:
 		0:
-			frame_cap_option_button.selected = 0
+			settings_frame_cap_option_button.selected = 0
 		1:
-			frame_cap_option_button.selected = 1
+			settings_frame_cap_option_button.selected = 1
 		2:
-			frame_cap_option_button.selected = 2
+			settings_frame_cap_option_button.selected = 2
 		3:
-			frame_cap_option_button.selected = 3
+			settings_frame_cap_option_button.selected = 3
 	if GameGlobals.game_settings.show_fps:
-		frame_label_option_button.selected = 1
+		settings_frame_label_option_button.selected = 1
 	else:
-		frame_label_option_button.selected = 0
+		settings_frame_label_option_button.selected = 0
 
 
 func _on_master_value_changed(value: float) -> void:
 	GameGlobals.audio_manager.create_audio("sound_menu_click")
-	master_label.text = str("%d" % [value])
+	settings_master_label.text = str("%d" % [value])
 	GameGlobals.game_settings.update_master_volume(value / 100)
 
 
 func _on_music_value_changed(value: float) -> void:
 	GameGlobals.audio_manager.create_audio("sound_menu_click")
-	music_label.text = str("%d" % [value])
+	settings_music_label.text = str("%d" % [value])
 	GameGlobals.game_settings.update_music_volume(value / 100)
 
 
 func _on_sound_value_changed(value: float) -> void:
 	GameGlobals.audio_manager.create_audio("sound_menu_click")
-	sound_label.text = str("%d" % [value])
+	settings_sound_label.text = str("%d" % [value])
 	GameGlobals.game_settings.update_sound_volume(value / 100)
 
 
