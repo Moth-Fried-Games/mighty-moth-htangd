034cc098fe99c8fa1f0735344d2f51f5d43bafc0
Cedi-Fonei <cedinfinity@gmail.com>
2026-07-24 11:34:58 -0400

    First pass at super meter handler and attempting to set up for lane binder

---
+++ game/main/lane_binders.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	#var all_lanebound_entities = owner.get_children().filter(func(child_node) -> bool:
+			#return child_node.has_member("lane_id"))
+#
+	#for laneitem in all_lanebound_entities:
+		#var lane_entity := laneitem as Node2D
+		#print(laneitem.name)
+		
+	return
---
+++ game/main/super_meter_handler.gd
@@ -0,0 +1,85 @@
+extends Node
+
+const on_punch_meter_gain: int = 1
+const on_deflect_meter_gain: int = 2
+const on_collect_meter_gain: int = 3
+
+var super_level: int = 0
+var super_meter: float = 0
+var combo_count: int = 0
+var combo_multiplier: float = 0
+
+const super_level_max: int = 3
+const super_meter_max: float = 100
+const combo_increment_on_punch: int = 1
+const combo_increment_on_deflect: int = 2
+const combo_multiplier_increment_threshold: int = 10
+const combo_multiplier_increment_value: float = 10
+const combo_multiplier_maximum: float = 10
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+	
+# Gives meter on a punch
+func on_successful_punch():
+	increment_combo(combo_increment_on_punch)
+	apply_meter_gain(on_punch_meter_gain)
+	pass
+	
+# Gives meter on a deflect
+func on_successful_deflect():
+	increment_combo(combo_increment_on_deflect)
+	apply_meter_gain(on_deflect_meter_gain)
+	pass
+	
+# Gives meter on a collect
+func on_successful_collect():
+	apply_meter_gain(on_collect_meter_gain)
+	pass
+	
+# Maths how much meter the player gets, accounting for their combo multiplier
+func calculate_meter_gain_value(base_gain_value):
+	var combo_bonus = (1.00 + 0.01 * combo_multiplier)/1
+	return base_gain_value * combo_bonus
+	
+# Increases combo counter and, if applicable, combo multiplier
+func increment_combo(increment_value):
+	var previous_combo_increment_threshold = combo_count % combo_multiplier_increment_threshold
+	
+	combo_count += increment_value
+	if combo_multiplier < combo_multiplier_maximum and combo_count % combo_multiplier_increment_threshold > previous_combo_increment_threshold:
+		combo_multiplier += combo_multiplier_increment_value
+	return
+
+# Applies meter gain, adjusting the super level as needed
+func apply_meter_gain(gain_value):
+	var adjusted_gain_value = calculate_meter_gain_value(gain_value)
+	
+	if super_level < super_level_max:
+		super_meter += adjusted_gain_value
+		
+		if super_meter >= super_meter_max:
+			super_level += 1
+			if super_level < super_level_max:
+				super_meter -= super_meter_max
+			else:
+				super_meter = super_meter_max
+	return
+
+# Removes all super levels, adjusting for the level-3 specific meter display as needed
+func expend_super_meter():
+	if super_level >= super_level_max:
+		super_meter = 0
+	super_level = 0
+	return
+
+# Breaks the player's combo count and multiplier
+func on_combo_break():
+	combo_count = 0
+	combo_multiplier = 0
