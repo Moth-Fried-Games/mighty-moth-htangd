567acc017f62c36a3c5fd3e7d3fd0a62c0cf0397
Cedi-Fonei <cedinfinity@gmail.com>
2026-07-26 13:07:26 -0400

    playing player animations

--- game/entities/player/player.gd
+++ game/entities/player/player.gd
@@ -5,6 +5,7 @@ enum PlayerState { IDLE, PUNCH, DEFLECT, COLLECT, SUPER_IDLE, SUPER_ATTACK, FINA
 
 const horizontal_offset_from_anchor = 150
 @onready var middle_left_anchor: Marker2D = $"../LaneBinders/Middle Lane/Middle Left Anchor"
+@onready var player_sprite: PlayerSprite = $PlayerSprite
 
 var cutscene: bool = false
 
@@ -27,14 +28,24 @@ func _physics_process(_delta: float) -> void:
 	if cutscene:
 		return
 	_movement()
+	_animate()
 
 
 func _movement() -> void:
 	if Input.is_action_just_pressed("move_up"):
 		_change_lane(direction.UP)
+		player_sprite.play("fly")
 	if Input.is_action_just_pressed("move_down"):
 		_change_lane(direction.DOWN)
-
+		player_sprite.play("fly")
+		
+func _animate() -> void:
+	if Input.is_action_just_pressed("punch"):
+		player_sprite.play("punch")
+	if Input.is_action_just_pressed("collect"):
+		player_sprite.play("collect")
+	if Input.is_action_just_pressed("deflect"):
+		player_sprite.play("deflect")
 
 func _on_super_input() -> void:
 	pass
