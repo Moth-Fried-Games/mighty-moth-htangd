3c17858ed94cf3dce8a53b41a317b93381a1881a
Myr the Moth <myrthemoth@gmail.com>
2026-07-26 02:27:19 +0100

    UI now pulls data from main scene

--- game/main/main_ui.gd
+++ game/main/main_ui.gd
@@ -6,14 +6,17 @@ extends CanvasLayer
 @onready var time_needle: TextureRect = %TimeNeedle
 @onready var time_warning: TextureRect = %TimeWarning
 
-@export_range(1, 3) var super_level: int = 1
+@export_range(0, 3) var super_level: int = 0
 @export_range(0, 100) var super_value: float = 0
-@export_range(0, 100) var super_minimums: Array[float] = [23, 53, 76]
-@export_range(0, 100) var super_maximums: Array[float] = [52, 75, 100]
+@export_range(0, 100) var super_minimums: Array[float] = [23, 53, 76, 100]
+@export_range(0, 100) var super_maximums: Array[float] = [52, 75, 100, 100]
 @export_range(0, 10) var souvenirs: int = 0
 @export_range(0, 100) var souvenir_minimum: float = 23
 @export_range(0, 100) var souvenir_maximum: float = 100
 @export_range(0, 60) var time_left: float = 60
+
+var main_scene: Node2D = null
+var super_scene: Node2D = null
 
 
 func _process(_delta: float) -> void:
@@ -25,17 +28,24 @@ func _process(_delta: float) -> void:
 
 func _update_values() -> void:
 	if not Engine.is_editor_hint():
-		pass
+		if not is_instance_valid(main_scene):
+			main_scene = get_parent()
+		if not is_instance_valid(super_scene):
+			super_scene = main_scene.super_meter_handler
+		super_level = clampi(super_scene.super_level, 0, 3)
+		super_value = clampf(super_scene.super_meter, 0, 100)
+		time_left = clampf(main_scene.game_over_timer.time_left, 0, 60)
+		souvenirs = clampi(main_scene.souvenirs_collected, 0, 10)
 
 
 func _update_super() -> void:
 	var super_interval: float = (
-		(super_maximums[super_level - 1] - super_minimums[super_level - 1]) * (super_value * 0.01)
+		(super_maximums[super_level] - super_minimums[super_level]) * (super_value * 0.01)
 	)
 	super_bar.value = clampf(
-		super_minimums[super_level - 1] + super_interval,
-		super_minimums[super_level - 1],
-		super_maximums[super_level - 1]
+		super_minimums[super_level] + super_interval,
+		super_minimums[super_level],
+		super_maximums[super_level]
 	)
 
 
