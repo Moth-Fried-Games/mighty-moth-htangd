8c7c3da4bad7cc67d066ae71fe1bcee7e25d1c0b
Cedi-Fonei <cedinfinity@gmail.com>
2026-07-31 14:13:35 -0400

    cleanup

--- game/entities/obstacles/collect_hit_box_area.gd
+++ game/entities/obstacles/collect_hit_box_area.gd
@@ -7,8 +7,8 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	if has_overlapping_areas():
-		if get_overlapping_areas().any(func(area: Area2D) -> bool: return area.name == "CollectHurtBoxArea"):
-			owner._on_collected()
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(_delta: float) -> void:
+	#if has_overlapping_areas():
+		#if get_overlapping_areas().any(func(area: Area2D) -> bool: return area.name == "CollectHurtBoxArea"):
+			#owner._on_collected()
--- game/entities/player/player_hurt_box.gd
+++ game/entities/player/player_hurt_box.gd
@@ -6,7 +6,6 @@ class_name PlayerHurtBox
 @onready var player_sprite: PlayerSprite = $"../PlayerSprite"
 
 var hurtful_objects_colliding : Array[Area2D]
-#var collectable_objects_colliding : Array[Area2D]
 
 var is_deflecting : bool = false
 
@@ -26,8 +25,6 @@ func _on_area_entered(area: Area2D) -> void:
 		if area.is_in_group("ParryHitBoxArea")  and is_deflecting:
 			player_sprite.play("deflect")
 			area.owner._on_deflected()
-		#elif area.is_in_group("Collectable"):
-			#collectable_objects_colliding.append(area)
 		elif area.is_in_group("HurtBoxArea"):
 			if !is_deflecting:
 				area.owner._on_touching_player()
@@ -35,8 +32,6 @@ func _on_area_entered(area: Area2D) -> void:
 				hurtful_objects_colliding.append(area)
 
 func _on_area_exited(area: Area2D) -> void:
-	#if area.has_method("collect") and collectable_objects_colliding.has(area):
-		#collectable_objects_colliding.erase(area)
 	if area is HurtBoxArea and hurtful_objects_colliding.has(area):
 		hurtful_objects_colliding.erase(area)
 
@@ -48,13 +43,8 @@ func _stop_deflect() -> void:
 			hurtful_objects_colliding.erase(object)
 
 func _physics_process(_delta: float) -> void:
-	#if Input.is_action_just_pressed("collect"):     
-		#if collectable_objects_colliding.size() > 0:
-			#player_sprite.play("collect")   
-		#for object in collectable_objects_colliding:
-			#if is_instance_valid(object):
-				#object.owner._on_collected()
-				#collectable_objects_colliding.erase(object)
 	if Input.is_action_just_pressed("deflect"):        
 		is_deflecting = true
 		timer.start()
+	else:
+		print("Heyo that timer? It's got... " + str(timer.wait_time) + " left.")
